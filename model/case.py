638da9863bc244b675d08c609451679362648529
Margaux LEFEVRE <lefevmar@gmail.com>
2018-06-20 11:33:15 +0200

    séparation des classes

---
+++ model/case.py
@@ -0,0 +1,11 @@
+#classe case
+class Case:
+
+    #constructeur
+    def __init__(self, valeur):
+        #attribut
+        self.valeur = []
+        print("Creation d'une case " + self.valeur)
+
+
+
---
+++ model/sudoku.py
@@ -0,0 +1,8 @@
+#classe sudoku
+class Sudoku:
+
+    #constructeur
+    def __init__(self, grille):
+        #attribut
+        self.grille = [][]
+        print("Creation d'un sudoku " + self.grille)
