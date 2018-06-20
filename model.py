55fc92b651d76de97f101608649a07f32d9a986a
Margaux LEFEVRE <lefevmar@gmail.com>
2018-06-20 11:07:04 +0200

    début des classes

---
+++ model.py
@@ -0,0 +1,20 @@
+#classe case
+class Case:
+
+    #constructeur
+    def __init__(self, valeur):
+        #attribut
+        self.valeur = []
+        print("Creation d'une case " + self.valeur)
+
+#classe sudoku
+class Sudoku:
+
+    #constructeur
+    def __init__(self, grille):
+        #attribut
+        self.grille = [][]
+        print("Creation d'un sudoku " + self.grille)
+
+
+
