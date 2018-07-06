72c801fdc2c0614190b9cbb4339027d9983e472c
MaxSenAgency <maxence.sendral@gmail.com>
2018-07-06 10:17:33 +0200

    Envoi UI + API interaction ok

--- Server/SudokuServer.py
+++ Server/SudokuServer.py
@@ -1,10 +1,30 @@
 import falcon
 import os
 from wsgiref import simple_server
+from falcon_cors import CORS
 
-from sudoku.Server.Routes import DataService
+# from Server.Routes import DataService
 
-app = falcon.API()
+cors = CORS(allow_origins_list=['http://127.0.0.1:8000/demo'])
+
+app = falcon.API(middleware=[cors.middleware])
+
+public_cors = CORS(allow_all_origins=True)
+
+class DataService:
+    cors = public_cors
+
+    def on_get(self, req, resp):
+        data = req.stream.read().decode('utf-8')
+        # output the data, we could write it to persistent storage here
+        resp.media = resolutionSudo
+        print('Resolution du Sudoku' + resolutionSudo)
+
+    def on_post(self, req, resp):
+        data = req.stream.read().decode('utf-8')
+        resp.media = "Everone can post to this resource" + data
+
+resolutionSudo = 'test'
 
 # Recupere le chemin du dossier courant
 dir_path = os.path.dirname(os.path.realpath(__file__))
