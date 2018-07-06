ba32008ee17e0464fe70c6b62b50b8ccc370550e
younsmjl <younes.manjal@gmail.com>
2018-07-06 10:19:31 +0200

    essai routes

--- Server/Routes.py
+++ Server/Routes.py
@@ -10,24 +10,20 @@ class DataService:
     def on_get(self, req, resp):
         data = req.stream.read().decode('utf-8')
         # output the data, we could write it to persistent storage here
-        resp.media = resolutionSudo
+        s = Sudoku()
+
+        # formate les données en format JSON
+        s.deserialisation(data)
+
+        # resoud le sudoku
+        s.solve()
+
+        # renvoie les données formater sur la page HTML
+        s.serialisation()
+
+        resp.media =  s.serialisation()
         print('Resolution du Sudoku' + resolutionSudo)
 
 
-
-# s = Sudoku()
-
-#formate les données en format JSON
-# s.deserialisation(data)
-
-# resoud le sudoku
-# s.solve()
-
-# renvoie les données formater sur la page HTML
-#s.serialisation()
-
-# resolutionSudo = s.serialisation()
-
-
-resolutionSudo = 'test'
+# resolutionSudo = 'test'
 
