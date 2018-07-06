5b729fa0ab056bd51d85b39d2843a8a921b9c090
younsmjl <younes.manjal@gmail.com>
2018-07-06 13:34:06 +0200

    ajout deserialisation et serialisation au server

--- Server/SudokuServer.py
+++ Server/SudokuServer.py
@@ -3,6 +3,7 @@ import os
 from wsgiref import simple_server
 from falcon_cors import CORS
 import urllib.parse
+from sudoku.model.sudoku import Sudoku
 
 # from Server.Routes import DataService
 
@@ -13,8 +14,10 @@ app = falcon.API(middleware=[cors.middleware])
 public_cors = CORS(allow_all_origins=True)
 
 class DataService:
+
     cors = public_cors
 
+    #TEST pour la page Demo vérifier qu'elle receptionne bien des informations
     def on_get(self, req, resp):
         data = req.stream.read().decode('utf-8')
         # output the data, we could write it to persistent storage here
@@ -22,11 +25,15 @@ class DataService:
         print('Resolution du Sudoku' + resolutionSudo)
 
     def on_post(self, req, resp):
-        data = req.bounded_stream.read().decode('utf-8')
+        data = urllib.parse.unquote(req.bounded_stream.read().decode('utf-8'))
         data = data.replace('json=','')
-        # urllib.parse.unquote(data)
-        resp.media = urllib.parse.unquote(data)
-        print(urllib.parse.unquote(data))
+
+        s = Sudoku()
+        # formate les données en format JSON
+        s.deserialisation(data)
+
+        resp.media = s.serialisation()
+
 
 resolutionSudo = 'test'
 
